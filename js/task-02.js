99e872eae50372db55c2e17dc907db3a71acfb8c
Alina Kornienko <a_kornienko@ukr.net>
2022-02-07 15:29:27 +0200

    Updated all tasks

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const createIngredientsList = options => {
+  return options.map(option => {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = option;
+  
+    return ingredientItem;
+    });
+};
+const elements = createIngredientsList(ingredients);
+ingredientsEl.append(...elements);
---
+++ js/task-09.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('button');
+const spanEl = document.querySelector('span');
+
+buttonEl.addEventListener('change', onBodyColorChange);
+
+function onBodyColorChange = (event) => {
+  const newColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = newColor;
+  spanEl.textContent = newColor;
+}
