b5bf10431dd7d45ba1c17854bd8d56a08c1ae804
Alina Kornienko <a_kornienko@ukr.net>
2022-02-07 15:44:25 +0200

    Updated task #1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,13 @@
 const categEl = document.querySelector('#categories');
 const itemsEl = categEl.querySelectorAll('.item');
-console.log(`В списке ${itemsEl.length} категории`);
+console.log('Nunber of categories:', categEl.length);
 console.log('');
 
 const titlesEl = [];
 itemsEl.forEach(elem => {
-  console.log('Category: ', elem.getElementsByTagName('h2')[0].textContent);
+  console.log('Category:', elem.getElementsByTagName('h2')[0].textContent);
   console.log(
-    'Elements: ',
+    'Elements:',
     elem.getElementsByTagName('li').length
   );
   console.log('');
