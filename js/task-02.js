eab25497346681fc4a20d24e0d77799b29db2835
Alina Kornienko <a_kornienko@ukr.net>
2022-02-07 14:09:35 +0200

    Updated task#2

--- js/task-02.js
+++ js/task-02.js
@@ -1,21 +1,8 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
 ];
-
-const ingredientsEl = document.querySelector('#ingredients');
-
-const createIngredientsList = options => {
-  return options.map(option => {
-    const ingredientItem = document.createElement('li');
-    ingredientItem.textContent = option;
-  
-    return ingredientItem;
-    });
-};
-const elements = createIngredientsList(ingredients);
-ingredientsEl.append(...elements);
